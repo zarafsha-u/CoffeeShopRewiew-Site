aa265962b68c124335b3b1c950ee8c32fbbe8300
zarafsha-u <zarafsha.u@gmail.com>
2022-10-16 12:55:25 -0400

    Config Var

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from forms import AddCafe
+import os
 
 app = Flask(__name__)
 Bootstrap(app)
-app.secret_key = '123'
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cafe_.sqlite"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db= SQLAlchemy(app)
 
